2d622405d0a50a2df751ab91d9ca3b008b946382
Haze <main@thayes.dev>
2022-11-18 00:35:09 -0600

    data processed

--- main.py
+++ main.py
@@ -10,11 +10,41 @@ import pandas as pd
 
 import json
 
-@flow(task_runner=SequentialTaskRunner)
-def data_process_flow(df):
-    logger = get_run_logger()
+def wind_direction(r):
+    '''
+    Takes in resistance value and returns wind direction
+    '''
+    
+
+    if r <= 200:
+        return 'E'
+    elif r > 200 & r <= 300:
+        return 'SE'
+    elif r > 300 & r <= 440:
+        return 'S'
+    elif r > 440 & r <= 750:
+        return 'NE'
+    elif r > 750 & r <= 1380:
+        return 'SW'
+    elif r > 1380 & r <= 2850:
+        return 'N'
+    elif r > 2850 & r <= 5000:
+        return 'NE'
+    elif r > 5000 & r <= 10000:
+        return 'E'
+
+def c_to_f(c):
+    return (((9*c)/5)+32)
 
 
+@task
+def process_data(df):
+
+    df['datetime'] = pd.to_datetime(df['datetime'], unit='s')
+    df['temperature'] = df['temperature'].apply(lambda x: c_to_f(x))
+    df['wind_direction'] = df['wind_direction'].apply(lambda x: wind_direction(x))
+
+    return df
 
 
 @flow(task_runner=SequentialTaskRunner)
@@ -42,7 +72,7 @@ def main_flow():
     df = pd.DataFrame()
     try:
         for message in consumer:
-            date_converted = message.value['datetime']
+            datetime = message.value['datetime']
             rain = message.value['rain']
             wind = message.value['wind']
             wind_direction = message.value['wind_direction']
@@ -50,7 +80,7 @@ def main_flow():
             pressure = message.value['pressure']
             humidity = message.value['humidity']
 
-            payload = [date_converted, rain, wind, wind_direction, temperature, pressure, humidity]
+            payload = [datetime, rain, wind, wind_direction, temperature, pressure, humidity]
 
             temp = pd.DataFrame([payload])
 
@@ -59,12 +89,12 @@ def main_flow():
         logger.error("Error while caching data!!!")
         logger.warning(e)
 
-    df.columns = ['datetime', 'rain', 'wind', 'wind_direction', 'tmp_temp', 'pressure', 'humidity']
+    df.columns = ['datetime', 'rain', 'wind', 'wind_direction', 'temperature', 'pressure', 'humidity']
     consumer.close()
 
     logger.info("Processing data...")
     try:
-        data_process_flow(df)
+        df = process_data(df)
     except Exception as e:
         logger.error("Error while processing data!!!")
 
